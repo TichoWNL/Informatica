3c5d0ee2efb649ea81cd613595f00d79bd4e0d05
TichoWNL <99634621+TichoWNL@users.noreply.github.com>
2025-10-15 12:13:19 +0200

    Add files via upload

---
+++ Import States.py
@@ -0,0 +1,11 @@
+import random
+
+# === GAME STATES ===
+Locatie = "Home"           
+sleutelGepakt = False
+noteGevonden = False        
+medicineGevonden = False
+basementUnlocked = False    
+gameOver = False            
+
+inventory = []
